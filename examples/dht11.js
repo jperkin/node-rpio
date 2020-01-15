049e1878decb5f29ae26cbeb6e7ed3c00e3aee11
Jonathan Perkin <jonathan@perkin.org.uk>
2020-01-15 22:42:54 +0000

    Overhaul the DHT11 example.
    This is verified to work reasonably well on both a Raspberry Pi 1 and 4,
    and with a lot of additional comments it should now be more useful to
    new users trying to understand what is going on.
    
    Fixes jperkin/node-rpio#109, at least for me.

--- examples/dht11.js
+++ examples/dht11.js
@@ -1,75 +1,154 @@
 var rpio = require('../lib/rpio');
 
 /*
- * Read a DHT11 attached to P07 / GPIO4 and print out the current temperature
- * and relative humidity if the read was successful.
+ * Repeatedly read a DHT11 attached to Pin 7 / GPIO 4 and print out the current
+ * temperature and relative humidity if the read was successful.
  */
 var pin = 7;
 
-var data = new Array(40);
-var buf = new Buffer(100000);
-var i;
+/*
+ * The DHT11 transmission is made up of 40 "bits", where the length of each
+ * high output determines whether it is a 0 or 1.  Per the datasheet, "0" is
+ * 26-28us in length, and "1" 70us.  However, in a JS environment we can't be
+ * so precise, and so we determine what is high and low based on a sampling of
+ * the values we received.
+ *
+ * These devices are notoriously unreliable, so this program will run forever
+ * attempting to retrieve and print valid data until the user kills the process.
+ *
+ * For the most portable implementation we read all bits after initialisation
+ * into a big buffer as fast as possible, and then parse afterwards.  This is
+ * necessary on e.g. the original Raspberry Pi 1 as the CPU is not fast enough
+ * to do something like:
+ *
+ *	while ((curstate = rpio.read(...)) == laststate)
+ *		count++;
+ *
+ * for each read, which would be the normal way of doing something like this.
+ */
+function read_dht11(vals)
+{
+	/* Data bits received from the DHT11 */
+	var data = new Array(40);
 
-/*
- * Initiate the MCU sequence.
- */
-rpio.open(pin, rpio.OUTPUT);
-rpio.write(pin, rpio.HIGH);
-rpio.msleep(500);
-rpio.write(pin, rpio.LOW);
-/*
- * The datasheet says 18us, but we need to account for the JavaScript function
- * call overhead.  Trial and error suggests this is a working value.
- */
-rpio.msleep(3);
-rpio.write(pin, rpio.HIGH);
-
-/*
- * Switch to input mode and read as fast as possible into our buffer.
- */
-rpio.mode(pin, rpio.INPUT);
-rpio.readbuf(pin, buf);
-
-/*
- * Parse the buffer for lengths of each high section.  The length determines
- * whether it's a low, high, or control bit.
- */
-var tmp = new Array(buf.length); /* 0.8 compat */
-for (i = 0; i < buf.length; i++) {
-	/* Convert buffer to array for node.js 0.8 compat */
-	tmp[i] = buf[i];
-}
-i = 0;
-tmp.join('').replace(/0+/g, '0').split('0').forEach(function(bits) {
 	/*
-	 * These are magic numbers.  If they don't work then uncomment this
-	 * line instead, it should give you the rough numbers required to
-	 * differentiate 0's and 1's.
-	 *
-	 * console.log(bits.length);
+	 * Our read buffer of all the bits sent.  Should be plenty.  On a
+	 * Raspberry Pi 4 a successful read uses up about half of this.
 	 */
-	if (bits.length > 320)
-		return;
-	data[i++] = (bits.length > 150) ? 1 : 0;
-});
+	var buf = new Buffer(50000);
+
+	/*
+	 * The initialisation sequence as per the datasheet is to start high,
+	 * pull low for at least 18ms, then back high and read.  The JavaScript
+	 * function call overhead ensures that we'll actually be sleeping
+	 * longer than that, but it doesn't appear to be a problem.
+	 */
+	rpio.open(pin, rpio.OUTPUT, rpio.HIGH);
+	rpio.write(pin, rpio.LOW);
+	rpio.msleep(18);
+
+	/*
+	 * Switch to input mode with a pull-up as per the datasheet, and read
+	 * as fast as possible into the buffer.  Configuring the pull-up is the
+	 * same as setting the line high first, so we don't explicitly do that,
+	 * to reduce the chance of missing the start of the transmission.
+	 */
+	rpio.mode(pin, rpio.INPUT, rpio.PULL_UP);
+	rpio.readbuf(pin, buf);
+	rpio.close(pin);
+
+	/*
+	 * The data has been received, split the buffer into groups of "1"s.
+	 */
+	var dlen = 0;
+	buf.join('').replace(/0+/g, '0').split('0').forEach(function(bits, n) {
+		/*
+		 * Skip past the first two values that are part of the
+		 * initialisation sequence sent by the DHT11 to indicate the
+		 * transmission is about to begin, and log the next 40 data
+		 * bits.
+		 */
+		if (n < 2 || n > 41)
+			return;
+
+		data[dlen++] = bits.length;
+	});
+
+	/*
+	 * In theory we should be able to test for exactly 40 bits here, but
+	 * sometimes only 39 bits are returned and the checksum is still valid,
+	 * so we allow it, but return early if any fewer than that.
+	 */
+	if (dlen < 39)
+		return false;
+
+	/*
+	 * Calculate the low and high water marks.  As each model of Raspberry
+	 * Pi will run at different speeds, the length of each high bit will
+	 * vary, so calculate the average and use that to determine what is
+	 * "high" and "low".
+	 *
+	 * The longest "low" seen on a Raspberry Pi 4 is around 135, so the
+	 * default low here should be more than sufficient.
+	 */
+	var low = 10000;
+	var high = 0;
+	for (var i = 0; i < dlen; i++) {
+		if (data[i] < low)
+			low = data[i];
+		if (data[i] > high)
+			high = data[i];
+	}
+	var avg = (low + high) / 2;
+
+	/*
+	 * The data received from the DHT11 is in 5 groups of 8-bits:
+	 *
+	 *	[0:7] integral relative humidity
+	 *	[8:15] decimal relative humidity
+	 *	[16:23] integral temperature
+	 *	[24:31] decimal temperature
+	 *	[32:39] checksum
+	 *
+	 * Parse the bitstream into the supplied "vals" buffer.
+	 */
+	vals.fill(0);
+	for (var i = 0; i < dlen; i++) {
+		var group = parseInt(i/8)
+
+		/* The data is in big-endian format, shift it in. */
+		vals[group] <<= 1;
+
+		/* This should be a high bit, based on the average. */
+		if (data[i] >= avg)
+			vals[group] |= 1;
+	}
+
+	/*
+	 * Validate the checksum and return whether successful or not.  The
+	 * checksum is simply the value of the other 4 groups combined, masked
+	 * off to 8-bits.
+	 */
+	return (vals[4] == ((vals[0] + vals[1] + vals[2] + vals[3]) & 0xFF));
+}
 
 /*
- * Pull out the individual value octets
+ * Run until the user kills the process, skipping any bad reads.
  */
-var rh_int = parseInt(data.slice(0, 8).join(''), 2);
-var rh_dec = parseInt(data.slice(8, 16).join(''), 2);
-var tm_int = parseInt(data.slice(16, 24).join(''), 2);
-var tm_dec = parseInt(data.slice(24, 32).join(''), 2);
-var chksum = parseInt(data.slice(32, 40).join(''), 2);
+var v = Buffer(5);
 
-/* chksum should match this calculation */
-var chk = ((rh_int + rh_dec + tm_int + tm_dec) & 0xFF);
+while (true) {
+	/*
+	 * On the DHT11 the fractional parts are always 0 so just print the
+	 * integrals.
+	 */
+	if (read_dht11(v)) {
+		console.log("Temperature = %dC, Humidity = %d%%", v[2], v[0]);
+	}
 
-/* Sometimes the checksum will be correct but the values obviously wrong. */
-if (chksum == chk && rh_int <= 100) {
-	console.log("Temperature = %dC, Humidity = %d%%", tm_int, rh_int);
-} else {
-	console.log("Read failed:");
-	console.log("    chk = %d, chksum = %d", chk, chksum);
-	console.log("    %d %d %d %d (%d = %d)", rh_int, rh_dec, tm_int, tm_dec, chk, chksum);
+	/*
+	 * The datasheet specifies that the sampling period should not be less
+	 * than 1 second, so be generous and give it 2.
+	 */
+	rpio.sleep(2);
 }
