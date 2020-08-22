c412b54bd5a0c83c0c6d37cff4889f777ff48d30
Jonathan Perkin <jonathan@perkin.org.uk>
2020-08-22 12:52:56 +0100

    Support Orange Pi Zero.

--- src/rpio.cc
+++ src/rpio.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Jonathan Perkin <jonathan@perkin.org.uk>
+ * Copyright (c) 2020 Jonathan Perkin <jonathan@perkin.org.uk>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
